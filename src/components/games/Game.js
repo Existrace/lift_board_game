31bf40c08ab6a136b65ae83094ca9a638083a300
brugautier <bru.gautier@gmail.com>
2021-10-05 22:58:17 +0200

    Squelette des différentes classes

---
+++ src/components/games/Game.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class Game {
+
+	const name;
+	
+	const pile;
+	
+	function getName() {
+		return name;
+	}
+	
+	function setName(newName) {
+		name = newName;
+	}
+	
+	function getPile() {
+		return pile;
+	}
+	
+	function setPile(newPile) {
+		pile = newPile;
+	}
+	
+	function addCard(card) {
+		// Fonction pour ajouter une carte à la pile
+		
+	}
+	
+	function drawCard() {
+		// Fonction pour piocher une carte
+		
+		
+		return card;
+	}	
+	
+	function mixCards() {
+		// Fonction pour mélanger la pile de cartes
+		
+	}
+} 
---
+++ src/components/games/Room.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class Room {
+
+	const code;
+
+	const players;
+	
+	const game;
+	
+	function getCode() {
+		return code;
+	}
+	
+	function setCode(newCode) {
+		code = newCode;
+	}
+	
+	function getPlayers() {
+		return players;
+	}
+	
+	function setPlayers(newPlayers) {
+		players = newPlayers;
+	}
+	
+	function getGame() {
+		return game;
+	}
+	
+	function setGame(newGame) {
+		game = newGame;
+	}
+	
+	function addPlayer(entryCode) {
+		// Fonction qui ajoute un joueur à la salle s'il a le bon code
+		
+	}
+} 
