128c1580972cca482cb3c8be7af84053e519afce
Snourry <30350968+Existrace@users.noreply.github.com>
2021-10-11 17:44:07 +0200

    Change view create room / Changements dans la vue 'create room'

--- src/components/website/CreateRoom.js
+++ src/components/website/CreateRoom.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Card, CardActions, CardContent, InputLabel, MenuItem, Select, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import CasinoIcon from "@mui/icons-material/Casino";
 import * as utils from '../../utils/utils_generate_code';
+import {Divider, Typography} from "@mui/material";
+
 
 /*List of all games available*/
 const games = [
@@ -13,10 +15,24 @@ function CreateRoom() {
 
     const [gameCode, setGameCode] = useState('')
 
+    const inputRef = React.useRef(null)
+
     function generateCode() {
         let random = utils.makeid(15)
         setGameCode(random)
     }
+
+    useEffect(() => {
+            generateCode()
+        }, []
+    )
+
+    const handleClick = () => {
+        /* Ne fonctionne pas encore */
+        const value = inputRef.current.value;
+        console.log(value)
+    }
+
 
     return (
         <div style={{
@@ -35,11 +51,17 @@ function CreateRoom() {
                 <CardContent>
                     <h1 style={{fontWeight: "bold", paddingBottom: 10, textAlign: "center"}}>Créer une salle</h1>
 
-                    <InputLabel id="demo-simple-select-label">Sélectionner jeu</InputLabel>
+                    <InputLabel id="demo-simple-select-label">Mot de passe</InputLabel>
+                    <TextField
+                                ref={inputRef}
+                               type="password">
+                    </TextField>
+
+                    <InputLabel style={{paddingTop: 20}} id="demo-simple-select-label">Sélectionner jeu</InputLabel>
                     <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        /*value={age}*/
+                        value={games[0]}
                         label="Selectionner jeu"
                         /*onChange={handleChange}*/
                     >
@@ -55,14 +77,13 @@ function CreateRoom() {
                     </Select>
                     <br/><br/>
 
-                    <TextField id="outlined-basic" disabled label="Code de la salle" variant="outlined" value={gameCode}
-                               onChange={(event) => setGameCode(event.target.value)}
-                    />
+                    <Typography variant="subtitle2" gutterBottom component="div" style={{paddingTop: 20}}>
+                        Code généré : <br/>
+                        {gameCode}
+                    </Typography>
 
                     <CardActions style={{paddingTop: 20}}>
-                        <Button style={{marginTop: 20}} size="small" variant="contained"
-                                onClick={generateCode}><CasinoIcon/> Générer code</Button>
-                        <Button style={{marginTop: 20}} variant="outlined">Créer salle de jeu</Button>
+                        <Button onClick={handleClick} style={{marginTop: 20}} variant="outlined">Créer salle de jeu</Button>
                     </CardActions>
                 </CardContent>
             </Card>
