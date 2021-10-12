65bc159bb1b9a183ef87cecc531e77800fb9803b
brugautier <bru.gautier@gmail.com>
2021-10-12 22:29:55 +0200

    Dynamisation du nom du jeu dans l'url de la carte à afficher

--- src/components/games/Card.js
+++ src/components/games/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Card = ({numCard}) => {
+const Card = ({numCard}, {gameName}) => {
+
+	function formatGameName(gameName) {
+		return gameName.toLowerCase().replace(" ", "-");
+	}
+
+	gameName = formatGameName(gameName);
 
 	return (
 		<div>
-			<img src={require(`../../img/detective-club/game_card/${numCard}.png`)}  alt={`card-${numCard}`}/>
+			<img src={require(`../../img/${gameName}/game_card/${numCard}.png`)}  alt={`card-${numCard}`}/>
 		</div>
 	);
 }
